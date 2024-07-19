30af3b65fae975a4ace30c342c5577f557429f97
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-07-19 00:13:54 -0300

    Fix toggle ui

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -3,14 +3,14 @@ import { Toggle } from './Toggle'
 import { CrownIcon } from '../icons/CrownIcon'
 
 export function Header() {
-  return (
-    <header className='w-full '>
-      <Link to='/leaderboard'>
-        <CrownIcon />
-      </Link>
-      <div className='fixed top-6 left-8 w-[104px]'>
-        <Toggle left={'EN'} right={'ES'} />
-      </div>
-    </header>
-  )
+	return (
+		<header className='w-full '>
+			<Link to='/leaderboard'>
+				<CrownIcon />
+			</Link>
+			<div className='fixed top-6 left-8 '>
+				<Toggle left={'EN'} right={'ES'} />
+			</div>
+		</header>
+	)
 }
--- src/components/home/Toggle.tsx
+++ src/components/home/Toggle.tsx
@@ -2,23 +2,23 @@ import { motion, spring } from 'framer-motion'
 import { useLanguages } from '../../hooks/useLanguage'
 
 export function Toggle({ left, right }: { left: string; right: string }) {
-  const { language, handleLanguages } = useLanguages()
+	const { language, handleLanguages } = useLanguages()
 
-  return (
-    <div className='animation flex items-center justify-between text-white '>
-      <span>{left}</span>
-      <div
-        onClick={handleLanguages}
-        data-language={language}
-        className='flex items-center justify-start w-[50px] h-[30px] bg-[#ffffffbb] rounded-2xl cursor-pointer data-[language=Spanish]:justify-end'
-      >
-        <motion.div
-          layout
-          transition={spring}
-          className='h-[25px] w-[25px] rounded-full bg-white'
-        ></motion.div>
-      </div>
-      <span>{right}</span>
-    </div>
-  )
+	return (
+		<div className='animation flex items-center justify-between text-white gap-2'>
+			<span>{left}</span>
+			<div
+				onClick={handleLanguages}
+				data-language={language}
+				className='flex items-center justify-start w-[50px] h-[30px] bg-[#ffffffbb] rounded-2xl cursor-pointer data-[language=Spanish]:justify-end'
+			>
+				<motion.div
+					layout
+					transition={spring}
+					className='h-[25px] w-[25px] rounded-full bg-white'
+				></motion.div>
+			</div>
+			<span>{right}</span>
+		</div>
+	)
 }
