cfda44673f06f15c1ca3e4568b4072faf4567429
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-07-21 20:06:28 -0300

    fix text mobile version

--- src/pages/Mobile.tsx
+++ src/pages/Mobile.tsx
@@ -5,11 +5,11 @@ export function MobileScreen() {
 	return (
 		<div className='absolute flex justify-center items-center w-full h-full backdrop-blur-[5px]'>
 			{language === 'English' ? (
-				<p className='text-[22px] text-[#f1f1f1] font-semibold'>
+				<p className='text-[22px] text-[#f1f1f1] font-semibold text-center'>
 					This application is only available on PC
 				</p>
 			) : (
-				<p className='text-[22px] text-[#f1f1f1] font-semibold'>
+				<p className='text-[22px] text-[#f1f1f1] font-semibold text-center'>
 					Esta aplicación esta solamente disponible en PC
 				</p>
 			)}
