716198e331f9dff018f25dbe0a99ac6e74bb9cf4
Enzo Sanchez <enzojsanchez98@gmail.com>
2024-08-31 15:54:38 -0300

    update settings

--- src/reducers/settings.ts
+++ src/reducers/settings.ts
@@ -2,33 +2,33 @@ import { DIFFICULTIES } from '../constants/difficulties'
 import { ACTION_TYPES_SETTINGS } from '../constants/actionTypesSettings'
 
 interface Settings {
-  name: string
-  difficulty: keyof typeof DIFFICULTIES
+	name: string
+	difficulty: keyof typeof DIFFICULTIES
 }
 
 type ActionSettingsTypes =
-  | { type: 'CHANGE_DIFFICULTY'; payload: keyof typeof DIFFICULTIES }
-  | { type: 'CHANGE_NAME'; payload: string }
+	| { type: 'CHANGE_DIFFICULTY'; payload: keyof typeof DIFFICULTIES }
+	| { type: 'CHANGE_NAME'; payload: string }
 
 export const initialSettings = {
-  name: '',
-  difficulty: DIFFICULTIES.Easy
+	name: '',
+	difficulty: DIFFICULTIES.Easy
 }
 
 export const settingsReducer = (
-  state: Settings,
-  action: ActionSettingsTypes
+	state: Settings,
+	action: ActionSettingsTypes
 ): Settings => {
-  const { type: actionType, payload: actionPayload } = action
-  switch (actionType) {
-    case ACTION_TYPES_SETTINGS.CHANGE_DIFFICULTY:
-      return {
-        ...state,
-        difficulty: actionPayload as keyof typeof DIFFICULTIES
-      }
-    case ACTION_TYPES_SETTINGS.CHANGE_NAME:
-      return { ...state, name: actionPayload }
-    default:
-      return state
-  }
+	const { type: actionType, payload: actionPayload } = action
+	switch (actionType) {
+		case ACTION_TYPES_SETTINGS.CHANGE_DIFFICULTY:
+			return {
+				...state,
+				difficulty: actionPayload as keyof typeof DIFFICULTIES
+			}
+		case ACTION_TYPES_SETTINGS.CHANGE_NAME:
+			return { ...state, name: actionPayload }
+		default:
+			return state
+	}
 }
