0ad8dd1d15d5245ae34866a92f326744b1c51229
Enzo Sanchez <enzojsanchez98@gmail.com>
2025-01-23 00:08:45 -0300

    fix change position

--- src/components/game/Playground.tsx
+++ src/components/game/Playground.tsx
@@ -5,13 +5,13 @@ import { Ball } from './Ball'
 import { CountDown } from './CountDown'
 
 export function Playground() {
-  const { position, handleClick } = usePosition()
+  const { position, changePosition } = usePosition()
   const { state } = useGame()
 
   return (
     <section className='relative h-4/5 w-[90%] flex justify-center items-center'>
       {state === STATES_GAME.PLAYING && (
-        <Ball handleClick={handleClick} position={position} />
+        <Ball handleClick={changePosition} position={position} />
       )}
       {state === STATES_GAME.COUNTING && <CountDown />}
     </section>
